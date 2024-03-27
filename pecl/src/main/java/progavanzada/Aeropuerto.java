fc95350d344d280e86f95c457884ad02b45ede68
Adrián Retamosa Gómez <123669073+ARetamosaG@users.noreply.github.com>
2024-03-27 11:10:26 +0100

    Más avances
    En verdad no sé si son avances o chapuzas, pero nuevas cosas hay

--- pecl/src/main/java/progavanzada/Aeropuerto.java
+++ pecl/src/main/java/progavanzada/Aeropuerto.java
@@ -1,10 +1,13 @@
 package progavanzada;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Aeropuerto {
     
     private String nombre;
+    private int viajeros;
 
     /* Estructuras de datos para las zonas de cada avión
         - ARRAYLIST --> CAPACIDAD ILIMITADA
@@ -18,6 +21,81 @@ public class Aeropuerto {
     private ArrayList<Avion> areaEstacionamiento = new ArrayList<>();
     private ArrayList<Avion> areaRodaje = new ArrayList<>();
     private Avion[] aerovias = new Avion[2];
+    
+    // Cola (FIFO) para las puertas de embarque:
+    Queue<Avion> colaPuertasEmbarque = new LinkedList<>();
+
+    public synchronized String getNombre() {
+        return nombre;
+    }
+
+    public synchronized void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public synchronized int getViajeros() {
+        return viajeros;
+    }
+
+    public synchronized void setViajeros(int viajeros) {
+        this.viajeros = viajeros;
+    }    
+
+    public synchronized ArrayList<Avion> getHangar() {
+        return hangar;
+    }
+
+    public synchronized void setHangar(ArrayList<Avion> hangar) {
+        this.hangar = hangar;
+    }
+
+    public synchronized Avion[] getTaller() {
+        return taller;
+    }
+
+    public synchronized void setTaller(Avion[] taller) {
+        this.taller = taller;
+    }
+
+    public synchronized Avion[] getPuertasEmbarque() {
+        return puertasEmbarque;
+    }
+
+    public synchronized void setPuertasEmbarque(Avion[] puertasEmbarque) {
+        this.puertasEmbarque = puertasEmbarque;
+    }
+
+    public synchronized Avion[] getPistas() {
+        return pistas;
+    }
+
+    public synchronized void setPistas(Avion[] pistas) {
+        this.pistas = pistas;
+    }
+
+    public synchronized ArrayList<Avion> getAreaEstacionamiento() {
+        return areaEstacionamiento;
+    }
+
+    public synchronized void setAreaEstacionamiento(ArrayList<Avion> areaEstacionamiento) {
+        this.areaEstacionamiento = areaEstacionamiento;
+    }
+
+    public synchronized ArrayList<Avion> getAreaRodaje() {
+        return areaRodaje;
+    }
+
+    public synchronized void setAreaRodaje(ArrayList<Avion> areaRodaje) {
+        this.areaRodaje = areaRodaje;
+    }
+
+    public synchronized Avion[] getAerovias() {
+        return aerovias;
+    }
+
+    public synchronized void setAerovias(Avion[] aerovias) {
+        this.aerovias = aerovias;
+    } 
     
     public Aeropuerto(String nombre){
         this.nombre = nombre;
--- pecl/src/main/java/progavanzada/Avion.java
+++ pecl/src/main/java/progavanzada/Avion.java
@@ -10,30 +10,41 @@ public class Avion extends Thread {
     private static final AtomicInteger varAtomAvion = new AtomicInteger(0);
     
     // Resto de atributos:
-    private String id;
-    private int maxPasajeros;
-    private Aeropuerto aeropuerto;
+    private final String id;
+    private final int maxPasajeros;
+    private Aeropuerto aeropuerto;   
     
     // Objeto random, usado en diferentes métodos:
     Random r = new Random();
-    
-    // Incrementa var. atómica, y la devuelve con ceros a la izquierda:
-    public int generarNumAvion(){
-        int numCompartido = varAtomAvion.incrementAndGet();
-        String numString = String.format("%04d", numCompartido);
-        return Integer.parseInt(numString);
+
+    public String getIdAvion() {
+        return id;
     }
 
-    // Genera las letras aleatorias del ID del avion:
-    public String generarLetrasAvion(){
+    public int getMaxPasajeros() {
+        return maxPasajeros;
+    }    
+
+    public Aeropuerto getAeropuerto() {
+        return aeropuerto;
+    }
+
+    public void setAeropuerto(Aeropuerto aeropuerto) {
+        this.aeropuerto = aeropuerto;
+    }    
+    
+    // Genera el ID completo del avión:
+    private String generarIdAvion() {
         char letra1 = (char) ('A' + r.nextInt(26));
         char letra2 = (char) ('A' + r.nextInt(26));
-        return String.valueOf(letra1) + String.valueOf(letra2);
+        int numCompartido = varAtomAvion.incrementAndGet();
+        String numString = String.format("%04d", numCompartido);
+        return letra1 + letra2 + "-" + numString;
     }
     
     // Constructor del objeto Avion:
     public Avion(Aeropuerto aeropuerto){
-        this.id = generarLetrasAvion() + "-" + generarNumAvion();
+        this.id = generarIdAvion();
         this.maxPasajeros = r.nextInt(201) + 100;
         this.aeropuerto = aeropuerto;
         
@@ -57,9 +68,36 @@ public class Avion extends Thread {
             oldAreaEst.add(this);
             this.aeropuerto.setAreaEstacionamiento(oldAreaEst);
             
-            //...
+            // Esperar a que toque el turno al avion:
+            while (this.aeropuerto.getColaPuertasEmbarque().peek() != this)
+                Thread.sleep(1000); // ESTO ES UN MIERDÓN PERO POR SEGUIR HACIENDO COSAS DEBAJO
             
-            // FALTAN MECANISMOS DE PROTECCIÓN DE DATOS
+            // Encontrar la puerta de embarque libre:
+            for (int i = 0; i <=6 && i != 2; i++){
+                Avion[] puertas = this.aeropuerto.getPuertasEmbarque();
+                if (puertas[i] == null){
+                    puertas[i] = this;
+                    this.aeropuerto.setPuertasEmbarque(puertas);
+                    break;
+                }
+            }
+            // TAMBIÉN ES UN POCO MOJÓN
+            
+            // Llenar el avión de pasajeros:
+            if (this.getMaxPasajeros() <= this.aeropuerto.getViajeros()){ // Hay suficientes
+                this.aeropuerto.setViajeros(this.aeropuerto.getViajeros() - this.getMaxPasajeros());
+                Thread.sleep(r.nextInt(3) + 1);
+            }
+            
+            else { // No hay suficientes
+                this.aeropuerto.setViajeros(this.aeropuerto.getViajeros() - this.getMaxPasajeros());
+                Thread.sleep(r.nextInt(3) + 1);
+                //Primera espera:
+                Thread.sleep(r.nextInt(5) + 1);
+                
+                // ...
+                
+            }
             
             
             
